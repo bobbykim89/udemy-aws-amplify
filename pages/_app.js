43450c57279278a9b317d843a3b81f90ddef7637
bobbykim89 <bobby.sihun.kim@gmail.com>
2022-05-06 01:26:31 -0700

    add navbar component and currently WIP

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import '@/configureAmplify'
+import Navbar from '@/components/Navbar'
+import { Fragment } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Navbar />
+      <div className='py-8 px-16 bg-slate-100'>
+        <Component {...pageProps} />
+      </div>
+    </Fragment>
+  )
 }
 
 export default MyApp
